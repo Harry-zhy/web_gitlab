bd0f657ab1327cfa6ca61302645483c9c3635c06
Teanna <txp219@student.bham.ac.uk>
2024-02-15 00:35:47 +0000

    Initial version of newteamprojectapp generated by generator-jhipster@7.9.4

---
+++ new-jhipster/src/test/java/com/teannateamproject/IntegrationTest.java
@@ -0,0 +1,22 @@
+package com.teannateamproject;
+
+import com.teannateamproject.NewteamprojectappApp;
+import com.teannateamproject.config.AsyncSyncConfiguration;
+import com.teannateamproject.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { NewteamprojectappApp.class, AsyncSyncConfiguration.class })
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
