824a5608b7d9e8bd8982d1cd107252964069bcf5
Asia Petrie <asiapetrie@Asias-MacBook-Air.local>
2024-03-09 04:09:32 +0000

    edited navbar

--- src/main/webapp/app/userprofile/userprofile.component.ts
+++ src/main/webapp/app/userprofile/userprofile.component.ts
@@ -34,58 +34,46 @@ export class UserprofileComponent implements OnInit {
   }
 
   isDateTime(event: Event) {
-    //console.log("in function");
     const dateTimeisChecked = event.target as HTMLInputElement;
     if (dateTimeisChecked.checked) {
       this.progress += 25;
-      //console.log("checked");
       console.log(this.progress);
     } else {
       if (this.progress > 0) {
         this.progress -= 25;
-        //console.log(this.progress);
       }
     }
   }
 
   isDecorations(event: Event) {
-    //console.log("in function");
     const decorationisChecked = event.target as HTMLInputElement;
     if (decorationisChecked.checked) {
       this.progress += 25;
-      //console.log(this.progress);
     } else {
       if (this.progress > 0) {
         this.progress -= 25;
-        //console.log(this.progress);
       }
     }
   }
 
   isVendors(event: Event) {
-    //console.log("in function");
     const vendorsisChecked = event.target as HTMLInputElement;
     if (vendorsisChecked.checked) {
       this.progress += 25;
-      //console.log(this.progress);
     } else {
       if (this.progress > 0) {
         this.progress -= 25;
-        //console.log(this.progress);
       }
     }
   }
 
   isVenue(event: Event) {
-    //console.log("in function");
     const venueisChecked = event.target as HTMLInputElement;
     if (venueisChecked.checked) {
       this.progress += 25;
-      //console.log(this.progress);
     } else {
       if (this.progress > 0) {
         this.progress -= 25;
-        //console.log(this.progress);
       }
     }
   }
