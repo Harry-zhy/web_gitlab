1ceab3b2f6c4bd59410f1771f38a74bcfb35fa18
Jiayang <TanfCha@outlook.com>
2024-03-19 04:04:50 +0000

    front end - Supplier almost finished (layout need to be done)

--- src/main/webapp/app/supplier-profile/supplier-profile.component.ts
+++ src/main/webapp/app/supplier-profile/supplier-profile.component.ts
@@ -7,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SupplierProfileComponent implements OnInit {
   constructor() {}
+  name: string[] = [];
+  price: number = 0;
+  capacity: string[] = [];
+  location: string[] = [];
+  showVenueExtraInfo = false;
+  showCookExtraInfo = false;
+  showCampExtraInfo = false;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // const coll: HTMLCollectionOf<Element> = document.getElementsByClassName('cook-info  ');
+    //
+    // for (let i = 0; i < coll.length; i++) {
+    //   coll[i].addEventListener('click', function (this: HTMLElement) {
+    //     this.classList.toggle('active');
+    //     const content: HTMLElement | null = this.nextElementSibling as HTMLElement;
+    //     if (content.style.display === 'none') {
+    //       content.style.display = 'block';
+    //     } else {
+    //       content.style.display = 'none';
+    //     }
+    //   });
+    // }
+  }
+
+  showVenue() {
+    this.showVenueExtraInfo = !this.showVenueExtraInfo;
+  }
+  showCook() {
+    this.showCookExtraInfo = !this.showCookExtraInfo;
+  }
+  showCamp() {
+    this.showCampExtraInfo = !this.showCampExtraInfo;
+  }
 }
