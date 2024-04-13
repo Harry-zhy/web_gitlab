7bb5a0e237b7305937f9a8a04b1a8e468111f92c
Asia Petrie <asiapetrie@Asias-MacBook-Air.local>
2024-04-13 15:41:20 +0100

    created modals for favourites page

--- src/main/webapp/app/favourites/favourites.component.ts
+++ src/main/webapp/app/favourites/favourites.component.ts
@@ -8,5 +8,34 @@ import { Component, OnInit } from '@angular/core';
 export class FavouritesComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.modalFunc();
+  }
+
+  modalFunc() {
+    var modal = document.getElementById('infoModal') as HTMLElement;
+
+    // Get the button that opens the modal
+    var btn = document.getElementById('expand') as HTMLElement;
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName('close')[0] as HTMLElement;
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+      modal.style.display = 'block';
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = 'none';
+    };
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+      }
+    };
+  }
 }
