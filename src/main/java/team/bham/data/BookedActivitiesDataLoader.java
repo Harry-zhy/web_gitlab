2c3eb390342d2bdc98e94fe4f3ce04e7176f2c8f
Karishma <kxs312@student.bham.ac.uk>
2024-04-20 18:47:09 +0100

    navbar change

--- src/main/java/team/bham/data/BookedActivitiesDataLoader.java
+++ src/main/java/team/bham/data/BookedActivitiesDataLoader.java
@@ -30,8 +30,22 @@ public class ActivitiesDataLoader implements ApplicationRunner {
         BookedActivity entity2 = new BookedActivity();
         entity2.setId(2L);
         entity2.setName("Music");
-        // Set other fields
         entityRepository.save(entity2);
+
+        BookedActivity entity3 = new BookedActivity();
+        entity3.setId(3L);
+        entity3.setName("Outdoor Adventure");
+        entityRepository.save(entity3);
+
+        BookedActivity entity4 = new BookedActivity();
+        entity4.setId(4L);
+        entity4.setName("Wine Tasting");
+        entityRepository.save(entity4);
+
+        BookedActivity entity5 = new BookedActivity();
+        entity5.setId(5L);
+        entity5.setName("Acting and Performances");
+        entityRepository.save(entity5);
         // Add more entities as needed
     }
 }
